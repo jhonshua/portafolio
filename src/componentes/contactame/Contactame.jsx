ca0ddfc401b3375c71ec769f1b347e8009ba1287
jhonshua <julio.llinas@gmail.com>
2023-01-13 14:08:51 -0400

    1/13/2023 1-modificacion

--- src/componentes/contactame/Contactame.jsx
+++ src/componentes/contactame/Contactame.jsx
@@ -15,30 +15,18 @@ const Contactame = () => {
 
        
 
-              <form action="https://formsubmit.co/julio.llinas@gmail.com" method="POST" className="contactame" >
-
-<<<<<<< HEAD
-            <div><img src={correo} className="correologo" />nombre<input type="text" name="name" className="nombre" required/><hr /></div> 
-            <div>correo<input type="text" name="correo" className="correo" required/><hr /></div>      
-            <div>mensaje <input type="text" name="mensaje"  className="mensaje" required/><hr /></div> 
-            <input type="hidden" name='_next' value="https://jhonshua.github.io/portafolio/" />
+              <form action="https://formsubmit.co/julio.llinas@gmail.com" method="POST" alt="no found" className="contactame" >
+              <div><img src={correo} className="correologo" />Nombre:<input type="text" name="name" className="nombre" required/><hr /></div>
+              <div>correo:<input type="text" name="correo" className="correo" required/><hr /></div>   
+              <div>mensaje:<input type="text" name="mensaje"  className="mensaje" required/><hr /></div> 
+              <input type="hidden" name='_next' value="https://jhonshua.github.io/portafolio/" />
             <input type="hidden" name='_captcha' value="false" />
-=======
-      <div><img src={correo} className="correologo" />nombre<input type="text" name="name" className="nombre" required/><hr /></div> 
-      <div>correo<input type="text" name="correo" className="correo" required/><hr /></div>      
-      <div>mensaje <input type="text" name="mensaje"  className="mensaje" required/><hr /></div> 
-      <input type="hidden" name='_next' value="https://jhonshua.github.io/portafolio/" />
-      <input type="hidden" name='_captcha' value="false" />
-      <button type="submit" class="btn btn-secondary">enviar</button>
-        </form>
->>>>>>> 87b41c0e3f65b76c303b9d26baee18b08ddf8993
 
             <button type="submit" className="btn btn-secondary">enviar</button>
             
               </form>
-
-    <div className='imagenes55'><a href="https://www.linkedin.com/in/julio-cesar-llinas-ba65a6127/" target="_blank"><img src={linkins} className='linkin' alt="no found" /></a><a href="https://drive.google.com/file/d/1VSRmz8hbn7NYHF1xGBTBiHnNyK1cnh9m/view?usp=share_link"  target="_blank"><img   className='pdf' src={pdf} alt="" /></a></div>
-
+              <div className='imagenes55'>  <a href="https://www.linkedin.com/feed/" target="_blank" alt="no found"><img src={linkins} className='linkin' alt="no found" /></a> <a href="https://drive.google.com/file/d/1VSRmz8hbn7NYHF1xGBTBiHnNyK1cnh9m/view?usp=share_link"  target="_blank"><img   className='pdf' src={pdf} alt="no found" /></a></div>
+    
 </div>
 
        
